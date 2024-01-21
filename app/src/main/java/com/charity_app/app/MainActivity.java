90d9c13ffb178e8dd8bb1520b9d593fb4126c506
Sameer Shaikh <sameershaikh01072004@gmail.com>
2024-01-21 16:24:27 +0530

    initial Commit

---
+++ app/src/main/java/com/charity_app/app/MainActivity.java
@@ -0,0 +1,14 @@
+package com.charity_app.app;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
